57de26b40d06c1a7c74dbf1a784a8d5f98990f2e
JD Nafziger <nafziger10@gmail.com>
2023-08-27 23:08:40 -0500

    updated read me and cleared extra comments and console logs out of server.js

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+# Server Side Calculator
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+I made a calculator that takes in data from the client and does the calculations on the server, making use of ajax POST and GET calls, exibiting the first two elements of CRUD.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+//  ✅  two inputs, one submit('='), 'C' clear button, +, -, * /
+//  ✅  client POST inputs
+//  ✅  server POST inputs
+//  ✅  server data structure
+//  ✅  History: Array of all previous calculations
+//  ✅  server perform math operations
+//                  Addition, Subtraction, Multiplication, and Division.
+//  ✅  server responds 'OK' 
+//  ✅  client GET req
+//  ✅  server GET
+//  ✅  client update DOM
+//  ✅  Clear button
+
+
+//STRETCH GOALS:
+    // ✅  convert interface to normal calc 
+          // ✅ number buttons
+
+
+
+
+
+
+
+
+<!-- Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md). -->
--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-// This must be added before GET & POST routes.
-// app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.urlencoded({ extended: true }));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
@@ -14,30 +12,17 @@ app.use(express.static('server/public'));
 let allCalcs = [];
 let calcCount = 0;
 
-//POST Routes go here Create Read Update Delete
-//                                PUT   DELETE   
-//                    POST   GET
+
 app.post('/calc', (req,res) => {
-  // console.log('calc recieved',req)
   req.body['result'] = calcToPerform(req.body);
   req.body['calcCount'] = calcCount;
   calcCount ++;
   allCalcs.push(req.body);
   res.sendStatus(201); // send back "Submitted"
 });
-// {
-// number1: 1,
-// number2: 1,
-// calc: +,
-// result: 2,
-// calcCount: 0
-
-// }
-
 
 
 function calcToPerform(info){
-  console.log(info);
   if (info.calc == '+'){
     return Number(info.number1) + Number(info.number2)
   }
@@ -54,7 +39,6 @@ function calcToPerform(info){
 
 // GET  Routes go here
 app.get('/calc', (req, res) => {
-  console.log('sending allCalcs')
   res.send(allCalcs);
 });
 
